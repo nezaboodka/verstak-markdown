3d3b39fce92612b94d49f357edb7e8e35f76a466
Yury Chetyrko <ychetyrko@gmail.com>
2022-11-13 00:45:28 +0300

    The latest Verstak

--- source/Markdown.v.ts
+++ source/Markdown.v.ts
@@ -18,5 +18,5 @@ const md = new Md({
 })
 
 export function Markdown(name: string, content: string) {
-  return HtmlText(md.render(content))
+  return HtmlText(md.render(content), name, { wrapping: true })
 }
